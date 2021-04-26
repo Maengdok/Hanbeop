4494a2c60efbdc5197006052817bd818deb57ee4
Maengdok <pionaxel@gmail.com>
2021-04-26 16:08:48 +0200

    see-more button

--- public/js/script.js
+++ public/js/script.js
@@ -1,25 +1,5 @@
-const isChecked = (id) => {
-    let button = document.getElementById("answer_" + id);
+const seeMore = (id) => {
+    let div = document.getElementById(id);
 
-    console.log(button);
-
-    if (sessionStorage.getItem('autosave')) {
-        button.value = sessionStorage.getItem('autosave');
-    }
-
-    button.addEventListener("change", function() {
-        sessionStorage.setItem('autotsave', button.value);
-    });
-
-    button.className == 'answer_button' ?  setToGreen(button) : setToWhite(button);
-}
-
-const setToGreen = (button) => {
-    button.className = 'answer_button checked';
-    button.style.background = 'green';
-}
-
-const setToWhite = (button) => {
-    button.className = 'answer_button';
-    button.style.background = 'white';
+    div.style.display == `none` ? div.style.display = `block` : div.style.display = `none`;
 }
